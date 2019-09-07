9c4a8ef0880be430d3365f300ef6aa9889d39e6b
Paul Hendrick <paul.g.hendrick@gmail.com>
2019-09-07 02:29:19 -0400

    🚚 Move in

---
+++ src/components/Header.svelte
@@ -0,0 +1,9 @@
+<style>
+    header {
+        padding: 2rem 2rem 0 2rem;
+    }
+</style>
+
+<header>
+      <b>Paul G. Hendrick</b>, <small><i>Web developer</i></small>
+</header>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,10 @@
-<style>
-
-</style>
-
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Paul G. Hendrick</title>
 </svelte:head>
 
+<p>Hello! This is my website.</p>
+<p>You can find me at <a href="https://github.com/pghk">github</a> and <a href="https://www.linkedin.com/in/paul-hendrick-3ab489168/">linkedin</a>.</p>
+<p>Thanks for stopping by.</p>
+<footer>
+    <p>🌎 2018</p>
+</footer>
