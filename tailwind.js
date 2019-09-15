d2bd2d47ee39438da54eab8243f89122ae1953d3
Paul Hendrick <paul.g.hendrick@gmail.com>
2019-09-14 22:35:58 -0400

    💄 Change color palette

--- tailwind.js
+++ tailwind.js
@@ -1,12 +1,11 @@
 module.exports = {
   theme: {
     fontFamily: {
-      mono: ['Consolas', 'Andale Mono', 'monospace', 'Courier'],
       'sans': ['Secuela'],
       'serif': ['Source Serif Variable'],
       'mono': ['Consolas', 'Andale Mono', 'monospace', 'Courier'],
     },
-    extend: {}
+    extend: { colors: require('open-color/open-color.json') }
   },
   variants: {},
   plugins: []
