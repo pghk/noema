1ef4b63cd0130fb57627d7c6955156a572d7c96d
Paul Hendrick <paul.g.hendrick@gmail.com>
2019-09-28 13:47:16 -0400

    Clean up font script

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -4,10 +4,10 @@ FONT_DIR='./static/fonts'
 SANS_SRC="SourceSansVariable-Roman"
 
 INPUT_FILE=($FONT_DIR/src/${SANS_SRC}.ttf)
-BASE_PARAMS=(--output-file\=$FONT_DIR/build/${SANS_SRC}.testing.woff)
+BASE_PARAMS=(--output-file=$FONT_DIR/build/${SANS_SRC}.testing.woff --flavor=woff --with-zopfli)
 
-SANS_PARAMS=(--flavor\=woff --with-zopfli --layout-features=c2sc --no-hinting --desubroutinize)
-SANS_CODES=(--unicodes\=U+41-5A,U+7B-7E)
+SANS_PARAMS=(--layout-features=c2sc --no-hinting --desubroutinize)
+SANS_CODES=(--unicodes=U+41-5A,U+7B-7E)
 
 set -x
 pyftsubset ${INPUT_FILE[@]} ${BASE_PARAMS[@]} ${SANS_PARAMS[@]} ${SANS_CODES[@]}
