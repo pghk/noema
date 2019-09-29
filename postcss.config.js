1a73363852da28a31738a6d5e0a3c7320e88f31f
Paul Hendrick <paul.g.hendrick@gmail.com>
2019-09-29 00:12:26 -0400

    🔧 Minify index.css

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,17 @@
 const tailwindcss = require("tailwindcss");
 
-// only needed if you want to purge
-const purgecss = require("@fullhuman/postcss-purgecss")({
+const purge = require("@fullhuman/postcss-purgecss")({
     content: ["./src/**/*.svelte", "./public/**/*.html"],
     defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+});
+
+const minify = require('cssnano')({
+    preset: 'default',
 });
 
 module.exports = {
     plugins: [
         tailwindcss("./tailwind.js"),
-
-        // only needed if you want to purge
-        ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+        ...(process.env.NODE_ENV === "production" ? [purge, minify] : [])
     ]
 };
