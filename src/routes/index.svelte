579073e5a8d9a8064f0e8e01b77eef149469c1b4
Paul Hendrick <paul.g.hendrick@gmail.com>
2019-11-09 12:51:50 -0500

    ✅ Update integration test

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,7 +9,8 @@
 	<meta name="Description" content="A fine web development.">
 </svelte:head>
 
-<p>Hello! This is my website.</p>
+<h1>Hello!</h1>
+<p>This is my website.</p>
 <p>You can find me at <a href="https://github.com/pghk">github</a> and <a href="https://www.linkedin.com/in/paul-hendrick-3ab489168/">linkedin</a>.</p>
 <p>Thanks for stopping by.</p>
 <footer>
