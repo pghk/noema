54483f4cbbbce9f07483824aa570457b34ebad75
Paul Hendrick <paul.g.hendrick@gmail.com>
2020-01-12 20:58:13 -0500

    Post-deploy func troubleshooting cont.

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -1,4 +1,4 @@
 exports.handler = async function(event, context) {
-    console.log("EVENT: \n" + JSON.stringify(event.name, null, 2))
+    console.log("EVENT: \n" + JSON.stringify(event.body.payload.name, null, 2))
     return context.logStreamName
 }
