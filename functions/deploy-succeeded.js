4bc175ac8e975886c8c9c219e50c3287ef408274
Paul Hendrick <paul.g.hendrick@gmail.com>
2020-01-12 20:31:36 -0500

    Partially revert post-deploy func
    Troubleshooting...

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -1,20 +1,4 @@
 exports.handler = async function(event, context) {
-    const {
-        id,
-        state,
-        name,
-        commit_ref,
-        branch,
-        title,
-        context
-    } = event.payload
-    console.log("ID: " + id + "\n"
-        + "STATE: " + state + "\n"
-        + "NAME: " + name + "\n"
-        + "COMMIT: " + commit_ref + "\n"
-        + "BRANCH: " + branch + "\n"
-        + "TITLE: " + title + "\n"
-        + "CONTEXT: " + context + "\n"
-    )
-    return 'test'
+    console.log("EVENT: \n" + JSON.stringify(event.payload.id, null, 2))
+    return context.logStreamName
 }
