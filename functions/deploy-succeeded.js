1d71ca1f10c698457763e905607ac91caa119c67
Paul Hendrick <paul.g.hendrick@gmail.com>
2020-01-13 00:43:28 -0500

    Use axios to make post request in node

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 exports.handler = async function(event, context, callback) {
     const eventData = JSON.parse(event.body).payload;
     let logMessage = `
@@ -14,15 +16,16 @@ exports.handler = async function(event, context, callback) {
     const TEST_SITE = "https://"
         + eventData.id + "--" + eventData.name + ".netlify.com";
 
-    await fetch(NOTIFY_URL, {
-        method: 'POST',
+    await axios({
+        method: 'post',
+        url: NOTIFY_URL,
         headers: {
             'Content-Type': 'application/json; charset=utf-8',
             'Accept': 'application/json',
             'Travis-API-Version': '3',
             'Authorization': `token ${process.env.TRAVIS_TOKEN}`,
         },
-        body: JSON.stringify({
+        data: JSON.stringify({
             'request': {
                 'branch': eventData.branch,
                 'message': `Netlify ${eventData.context}`,
