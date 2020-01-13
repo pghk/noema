13473bce3b6a0a672df82c049b40db8184cedcce
Paul Hendrick <paul.g.hendrick@gmail.com>
2020-01-12 19:44:32 -0500

    Extract relevent event details in post-deploy func

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -1,4 +1,20 @@
-exports.handler =  async function(event, context) {
-    console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-    return context.logStreamName
+exports.handler = async function(event, context) {
+    const {
+        id,
+        state,
+        name,
+        commit_ref,
+        branch,
+        title,
+        context
+    } = event.payload
+    console.log("ID: " + id + "\n"
+        + "STATE: " + state + "\n"
+        + "NAME: " + name + "\n"
+        + "COMMIT: " + commit_ref + "\n"
+        + "BRANCH: " + branch + "\n"
+        + "TITLE: " + title + "\n"
+        + "CONTEXT: " + context + "\n"
+    )
+    return 'test'
 }
