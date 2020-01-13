09a080b665f13749cc8665c06fb147f5381b3195
Paul Hendrick <paul.g.hendrick@gmail.com>
2020-01-12 21:28:57 -0500

    Fix post-deploy script 🤞

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -1,5 +1,20 @@
 exports.handler = async function(event, context) {
-    const info = JSON.parse(event.body)
-    console.log("TEST: " + info.payload.name + "\n")
-    return 'testing'
+    const {
+        id,
+        state,
+        name,
+        commit_ref,
+        branch,
+        title,
+        context
+    } = JSON.parse(event.body).payload
+    console.log("\n -- NETLIFY INFO -- \n"
+        + "ID: " + id + "\n"
+        + "STATE: " + state + "\n"
+        + "NAME: " + name + "\n"
+        + "COMMIT: " + commit_ref + "\n"
+        + "BRANCH: " + branch + "\n"
+        + "TITLE: " + title + "\n"
+        + "CONTEXT: " + context + "\n"
+    )
 }
