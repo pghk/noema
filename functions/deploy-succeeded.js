8493e23d4fea0d47bdddfaa8c8809b4634a41fd7
Paul Hendrick <paul.g.hendrick@gmail.com>
2020-01-13 00:24:57 -0500

    Trigger E2E tests in Travis on Netlify deploy

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -1,5 +1,5 @@
-exports.handler = async function(event, context) {
-    const eventData = JSON.parse(event.body).payload
+exports.handler = async function(event, context, callback) {
+    const eventData = JSON.parse(event.body).payload;
     let logMessage = `
     -- ${eventData.name} ${eventData.context} ${eventData.id} --
     title: ${eventData.title}
@@ -7,5 +7,40 @@ exports.handler = async function(event, context) {
     commit: ${eventData.commit_ref}
     `;
     console.log(logMessage);
-    return 'ok'
+
+    const NOTIFY_URL = "https://api.travis-ci.com/repo/"
+        + process.env.GIT_USER + "%2F" + eventData.name + "/requests";
+
+    const TEST_SITE = "https://"
+        + eventData.id + "--" + eventData.name + ".netlify.com";
+
+    await fetch(NOTIFY_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+            'Accept': 'application/json',
+            'Travis-API-Version': '3',
+            'Authorization': `token ${process.env.TRAVIS_TOKEN}`,
+        },
+        body: JSON.stringify({
+            'request': {
+                'branch': eventData.branch,
+                'message': `Netlify ${eventData.context}`,
+                'config': {
+                    'env': { 'CYPRESS_baseUrl': TEST_SITE }
+
+                }
+            }
+        })
+    })
+        .then(res => res.json())
+        .then(json => console.log(json))
+        .catch(err => {
+            console.log(err);
+            return callback(err);
+        });
+
+    return callback(null, {
+        statusCode: 200
+    });
 };
