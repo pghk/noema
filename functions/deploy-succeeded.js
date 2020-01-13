1761b21f7f351aa92bf48c37fb8f43c535b1ef0a
Paul Hendrick <paul.g.hendrick@gmail.com>
2020-01-12 22:13:57 -0500

    Tidy up post-deploy func log

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -1,14 +1,14 @@
 exports.handler = async function(event, context) {
     const eventData = JSON.parse(event.body).payload
     let logMessage = `
-        -- NETLIFY INFO --
-        ID: ${eventData.id}
-        STATE: ${eventData.state}
-        NAME: ${eventData.name}
-        COMMIT: ${eventData.commit_ref}
-        BRANCH: ${eventData.branch}
-        TITLE: ${eventData.title}
-        CONTEXT: ${eventData.context}
+    -- BUILD INFO --
+    CONTEXT: ${eventData.context}
+    ID: ${eventData.id}
+    TITLE: ${eventData.title}
+    NAME: ${eventData.name}
+    BRANCH: ${eventData.branch}
+    COMMIT: ${eventData.commit_ref}
+    STATE: ${eventData.state}
     `;
     console.log(logMessage);
     return 'ok'
