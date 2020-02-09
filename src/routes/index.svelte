d5c2c634deb45f6f1acce23daea13061aeea3310
Paul Hendrick <paul.g.hendrick@gmail.com>
2020-02-08 23:12:31 -0500

    ✨ Add a footer component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,6 +13,3 @@
 <p>This is my website.</p>
 <p>You can find me at <a href="https://github.com/pghk">github</a> and <a href="https://www.linkedin.com/in/paul-hendrick-3ab489168/">linkedin</a>.</p>
 <p>Thanks for stopping by.</p>
-<footer>
-    <p>🌎 2018</p>
-</footer>
