8b3478e82818d01e5fba8b553e21f3cd6dfbd70e
Paul Hendrick <paul.g.hendrick@gmail.com>
2020-03-03 20:06:45 -0500

    ✨ Gather real git info for footer props

--- scripts/buildInfoLoader.js
+++ scripts/buildInfoLoader.js
@@ -1,10 +1,18 @@
 const svelte = require('svelte/compiler');
+const { execSync } = require('child_process');
+
+const git = cmd => execSync(`git ${cmd}`).toString().trim();
+
+const commitHash = git('rev-parse HEAD');
+const commitDate = new Date(git('log -1 --format="%ci"'));
+
+const format = (date, options) => date.toLocaleDateString('en-US', options);
 
 const buildInfo = {
-    "MONTH": "06",
-    "DAY": "18",
-    "YEAR": "1982",
-    "HASH": "aef3c46023a5da88b112dc9ce1b90dce02d6ecb9"
+    "MONTH": format(commitDate, {month: '2-digit'}),
+    "DAY": format(commitDate, {day: '2-digit'}),
+    "YEAR": format(commitDate, {year: 'numeric'}),
+    "HASH": commitHash
 };
 const regex = new RegExp(Object.keys(buildInfo).join('|'), 'g');
 
