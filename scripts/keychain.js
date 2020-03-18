e01bff5ccdc45a5a05535ee44858f72d38449146
Paul Hendrick <paul.g.hendrick@gmail.com>
2020-03-17 23:54:58 -0400

    🏗 Set keychain-aquired API key as environment var
    Instead of returning a promise which resolves to the API key, this
    script now does something completely different:
    
    Takes the name of an npm script as argument, then spawns that script in
    a child process where the API key has been made available.
    
    This allows me to continue storing my secrets used by the dev
    environment in my OS keychain (as opposed to plaintext .env), without
    needing to require keytar or dotenv inside any scripts that become part
    of the webpack bundle.
    
    That, in turn, eliminates the need for me to write consumed API data to
    a file, opening an opportunity to make API calls more directly.

--- scripts/keychain.js
+++ scripts/keychain.js
@@ -1,6 +1,15 @@
 require('dotenv').config();
 const keychain = require('keytar');
+const {spawn} = require('child_process');
 
-module.exports = () => {
-    return keychain.getPassword(process.env.KC_SERVICE, process.env.KC_ACCOUNT);
-};
+keychain.getPassword(process.env.KC_SERVICE, process.env.KC_ACCOUNT).then(r => {
+  process.env.AIRTABLE_API_KEY = r;
+  console.log('Airtable key initialized.');
+  const subprocess = spawn('npm', ['run', process.argv[2]], {stdio: 'inherit'});
+  subprocess.on('error', (err) => {
+    console.error('Failed to start subprocess.');
+  });
+}).catch(e => {
+  console.error(e);
+  process.exitCode = 1;
+});
