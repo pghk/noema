d43801ad68cd98cdcbbc553de6f610757832b225
Paul Hendrick <paul.g.hendrick@gmail.com>
2020-08-26 21:57:54 -0400

    🔧 Separate config files for Cypress local/remote

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,10 +11,21 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+// promisified fs module
+const fse = require('fs-extra')
+const path = require('path')
+
+function getConfigurationFor (env) {
+  const pathToConfigFile = path.resolve('cypress', 'config', `${env}.json`)
+  return fse.readJson(pathToConfigFile)
+}
+
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  require('@cypress/code-coverage/task')(on, config)
-  on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'))
-  // `config` is the resolved Cypress config
+  if (process.env.CYPRESS_baseUrl) {
+    config = getConfigurationFor('remote');
+  } else {
+    require('@cypress/code-coverage/task')(on, config)
+    on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'))
+  }
   return config
 }
